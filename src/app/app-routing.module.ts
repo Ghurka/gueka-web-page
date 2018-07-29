99578bc718795b385672fed23d0c487dc8569880
Marco Capo <marco.capo@globallogic.com>
2018-07-29 03:00:30 -0300

    Added version as env variable

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { FooterComponent }      from './footer/footer.component';
+
+const routes: Routes = [
+  //{ path: 'footer', component: FooterComponent }
+];
+
+@NgModule({
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
+})
+export class AppRoutingModule { }
